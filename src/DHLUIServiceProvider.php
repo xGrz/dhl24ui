84016fc5c4b19f260148be495bd7cd8f30d8b8a0
xGrz <33755434+xGrz@users.noreply.github.com>
2024-05-20 14:06:59 +0200

    Setup package

---
+++ src/DHLUIServiceProvider.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace xGrz\Dhl24UI;
+
+
+use Illuminate\Support\ServiceProvider;
+
+class DHLUIServiceProvider extends ServiceProvider
+{
+
+
+    public function register(): void
+    {
+    }
+
+    public function boot(): void
+    {
+    }
+
+
+}
