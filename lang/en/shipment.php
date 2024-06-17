11bc47b1b96d7bcb54513e2b69aa900641409f43
xGrz <33755434+xGrz@users.noreply.github.com>
2024-06-17 09:51:13 +0200

    Added EN lang translations

---
+++ lang/en/shipment.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    'services' => [
+        'content' => 'Content',
+        'costsCenter' => 'Costs center',
+        'comment' => 'Comment',
+        'value' => 'Value',
+        'cod' => 'Cash on delivery',
+        'reference' => 'Reference',
+        'pdi' => 'Pre delivery information',
+        'rod' => 'Return on delivery',
+        'owl' => 'Pickup at the terminal'
+    ],
+    'attributes' => [
+        'quantity' => 'Quantity',
+        'weight' => 'Weight',
+        'width' => 'Width',
+        'height' => 'Height',
+        'length' => 'Length',
+        'nonStandard' => 'Non-standard'
+    ]
+];
