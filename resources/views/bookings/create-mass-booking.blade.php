cef7f5e56a3f4a8dd2ae102b139264a9563e52d4
xGrz <33755434+xGrz@users.noreply.github.com>
2024-06-18 12:11:03 +0200

    Added postal code finder

--- resources/views/bookings/create-mass-booking.blade.php
+++ resources/views/bookings/create-mass-booking.blade.php
@@ -1,4 +1,5 @@
-<div>
+@php use Illuminate\Support\Carbon; @endphp
+<div class="grid gap-2">
     <x-p-paper>
         <x-slot:title>Select shipments</x-slot:title>
         <x-p-table>
@@ -30,5 +31,44 @@
         <div class="text-center mt-4 mb-2">
             <x-p-button disabled="{{!$bookingEnabled}}" wire:click="book">Book courier</x-p-button>
         </div>
+        @json($postalCode)
     </x-p-paper>
+{{--    @if($shipmentList)--}}
+{{--        <div class="grid grid-cols-1 gap-2">--}}
+{{--            <x-p-paper>--}}
+{{--                <x-p-select label="Pickup date" wire:model.live="pickupDate">--}}
+{{--                    @foreach($dateOptions as $pickupDateOption)--}}
+{{--                        <option value="{{$pickupDateOption}}">--}}
+{{--                            {{$pickupDateOption}} ({{Carbon::parse($pickupDateOption)->dayName}})--}}
+{{--                        </option>--}}
+{{--                    @endforeach--}}
+{{--                </x-p-select>--}}
+{{--                @if (!Carbon::parse($pickupDate)->isToday())--}}
+{{--                    <div class="text-yellow-500">You book a courier with a future date</div>--}}
+{{--                @endif--}}
+{{--            </x-p-paper>--}}
+
+{{--            <x-p-paper>--}}
+{{--                <x-p-select label="Pickup from" wire:model.live="pickupFrom" wire:key="{{$pickupDate.$pickupFrom}}">--}}
+{{--                    @foreach($pickupFromOptions as $key => $from)--}}
+{{--                        <option>{{$from}}</option>--}}
+{{--                    @endforeach--}}
+{{--                </x-p-select>--}}
+{{--            </x-p-paper>--}}
+
+{{--            <x-p-paper>--}}
+{{--                <x-p-select label="Pickup to" wire:model.live="pickupTo" wire:key="{{$pickupDate.$pickupFrom.$pickupTo}}">--}}
+{{--                    @foreach($pickupToOptions as $key => $to)--}}
+{{--                        <option>{{$to}}</option>--}}
+{{--                    @endforeach--}}
+{{--                </x-p-select>--}}
+{{--            </x-p-paper>--}}
+
+{{--            <div class="text-center">--}}
+{{--                <x-p-button size="large" wire:click="book">Book</x-p-button>--}}
+{{--            </div>--}}
+{{--        </div>--}}
+{{--    @endif--}}
+{{--    <div wire:loading>Loading...</div>--}}
+
 </div>
--- src/Livewire/CreateMassBooking.php
+++ src/Livewire/CreateMassBooking.php
@@ -4,15 +4,21 @@ namespace xGrz\Dhl24UI\Livewire;
 
 use Illuminate\Database\Eloquent\Collection;
 use Livewire\Component;
-use xGrz\Dhl24\Facades\DHL24;
 use xGrz\Dhl24\Models\DHLShipment;
 
 class CreateMassBooking extends Component
 {
 
     public Collection $shipments;
+    public string|null $postalCode = null;
     public array $shipmentList = [];
     public bool $selectedAll = false;
+    public array $dateOptions = [];
+    public array $pickupFromOptions = [];
+    public array $pickupToOptions = [];
+    public string $pickupDate = '';
+    public string $pickupFrom = '';
+    public string $pickupTo = '';
 
     public function mount()
     {
@@ -23,14 +29,10 @@ class CreateMassBooking extends Component
             ->get();
     }
 
-    public function book()
-    {
-        $bookings = DHL24::booking()->options($this->shipments->first()->shipper_postal_code, $this->shipments);
-        dd( $bookings->getAvailableBookings(), $bookings->availableDates());
-    }
 
     public function render()
     {
+        self::findPostalCode();
         return view('dhl-ui::bookings.create-mass-booking', [
             'bookingEnabled' => (bool)count($this->shipmentList)
         ]);
@@ -48,6 +50,27 @@ class CreateMassBooking extends Component
             return;
         }
         $this->shipmentList = $this->shipments->map(fn($shipment) => $shipment->number)->toArray();
+    }
 
+    private function findPostalCode(): void
+    {
+        if (!count($this->shipmentList)) {
+            self::resetBooking();
+            return;
+        }
+
+        $this->postalCode = $this->shipments
+            ->filter(fn($shipment) => $shipment->number === (int) $this->shipmentList[0])
+            ?->first()
+            ->shipper_postal_code;
+    }
+
+    private function resetBooking(): void
+    {
+        $this->postalCode = null;
+        $this->pickupFromOptions = [];
+        $this->pickupToOptions = [];
+        $this->pickupFrom = '';
+        $this->pickupTo = '';
     }
 }
