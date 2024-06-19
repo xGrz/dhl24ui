db660c38ca2fbb167fb0f202f187c48cd9ed73ba
xGrz <33755434+xGrz@users.noreply.github.com>
2024-06-19 14:17:27 +0200

    [FIX] Date and time selector for booking moved to separate view.
    This view is shared between single and mass booking.

--- resources/views/bookings/create-mass-booking.blade.php
+++ resources/views/bookings/create-mass-booking.blade.php
@@ -1,4 +1,3 @@
-@php use Illuminate\Support\Carbon; @endphp
 <div class="grid gap-2">
     <x-p-paper>
         <x-slot:title>Select shipments</x-slot:title>
@@ -42,44 +41,9 @@
         </x-p-table>
     </x-p-paper>
     @if($postalCode)
-        <div class="grid grid-cols-4 gap-2">
-            <x-p-paper class="col-span-2">
-                <x-p-select label="Pickup date" wire:model.live="pickupDate">
-                    @foreach($dateOptions as $pickupDateOption)
-                        <option value="{{$pickupDateOption}}">
-                            {{$pickupDateOption}} ({{Carbon::parse($pickupDateOption)->dayName}})
-                        </option>
-                    @endforeach
-                </x-p-select>
-                @if (!Carbon::parse($pickupDate)->isToday())
-                    <div class="text-yellow-500">You book a courier with a future date</div>
-                @endif
-            </x-p-paper>
-
-            <x-p-paper>
-                <x-p-select label="Pickup from" wire:model.live="pickupFrom" wire:key="{{$pickupDate.$pickupFrom}}">
-                    @foreach($pickupFromOptions as $key => $from)
-                        <option>{{$from}}</option>
-                    @endforeach
-                </x-p-select>
-            </x-p-paper>
-
-            <x-p-paper>
-                <x-p-select label="Pickup to" wire:model.live="pickupTo" wire:key="{{$pickupDate.$pickupFrom.$pickupTo}}">
-                    @foreach($pickupToOptions as $key => $to)
-                        <option>{{$to}}</option>
-                    @endforeach
-                </x-p-select>
-            </x-p-paper>
-            <x-p-paper class="col-span-4">
-                <x-p-input label="Info" wire:model="info" />
-            </x-p-paper>
-
-            <div class="text-center col-span-4">
-                <x-p-button size="large" wire:click="book" disabled="{{!$postalCode}}">Book</x-p-button>
-            </div>
+        <div>
+            @include('dhl-ui::bookings.bookingDateForm')
         </div>
     @endif
     <div wire:loading>Loading...</div>
-
 </div>
--- resources/views/shipments/livewire/create-single-shipment-booking.blade.php
+++ resources/views/shipments/livewire/create-single-shipment-booking.blade.php
@@ -1,35 +1,4 @@
 @php use Illuminate\Support\Carbon; @endphp
 <div class="grid grid-cols-1 gap-2">
-    <x-p-paper>
-        <x-p-select label="Pickup date" wire:model.live="pickupDate">
-            @foreach($dateOptions as $pickupDateOption)
-                <option value="{{$pickupDateOption}}">
-                    {{$pickupDateOption}} ({{Carbon::parse($pickupDateOption)->dayName}})
-                </option>
-            @endforeach
-        </x-p-select>
-        @if (!Carbon::parse($pickupDate)->isToday())
-            <div class="text-yellow-500">You book a courier with a future date</div>
-        @endif
-    </x-p-paper>
-
-    <x-p-paper>
-        <x-p-select label="Pickup from" wire:model.live="pickupFrom" wire:key="{{$pickupDate.$pickupFrom}}">
-            @foreach($pickupFromOptions as $key => $from)
-                <option>{{$from}}</option>
-            @endforeach
-        </x-p-select>
-    </x-p-paper>
-
-    <x-p-paper>
-        <x-p-select label="Pickup to" wire:model.live="pickupTo" wire:key="{{$pickupDate.$pickupFrom.$pickupTo}}">
-            @foreach($pickupToOptions as $key => $to)
-                <option>{{$to}}</option>
-            @endforeach
-        </x-p-select>
-    </x-p-paper>
-
-    <div class="text-center">
-        <x-p-button size="large" wire:click="book">Book</x-p-button>
-    </div>
+    @include('dhl-ui::bookings.bookingDateForm')
 </div>
